600ae2f4bb5925ba6835ab98cd76f050e8d6222e
Ksenia Sakharova <nagis38@gmail.com>
2024-08-31 18:56:11 +0300

    Изменить стилистику цветов

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -15,9 +15,9 @@ export const Navbar: FC<Props> = ({ className = '' }) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
 
-      <div>
-        <AppLink className={cls.link} to={'/'}>{t('главная')}</AppLink>
-        <AppLink className={cls.link} to={'/about'}>{t('о сайте')}</AppLink>
+      <div className={cls.links}>
+        <AppLink to={'/'}>{t('главная')}</AppLink>
+        <AppLink to={'/about'}>{t('о сайте')}</AppLink>
       </div>
     </div>
   );
